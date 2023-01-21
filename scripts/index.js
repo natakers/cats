5fd95e09c233701961695a4653ff311b51f552b5
natakers <n.kersnovskaya@gmail.com>
2023-01-21 20:34:25 +0300

    feat:cats_from_file

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
+let main = document.querySelector("main");
+cats.forEach(function (cat) {
+  let card = `<div class="${
+    cat.favourite ? "card like" : "card"
+  }" style="background-image: url(${cat.img_link})">
+    <span>${cat.name}</span>
+    </div>`;
+  main.innerHTML += card;
+});
+
 let cards = document.getElementsByClassName("card");
+
 for (let i = 0, cnt = cards.length; i < cnt; i++) {
-const width = cards[i].offsetWidth;
-cards[i].style.height = width * 0.6 + "px";
+  const width = cards[i].offsetWidth;
+  cards[i].style.height = width * 0.6 + "px";
 }
