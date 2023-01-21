faca08a6197087ea27ad0005da49e6843bde8cba
natakers <n.kersnovskaya@gmail.com>
2023-01-21 20:42:17 +0300

    feat:cats_from_db

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,24 @@
 let main = document.querySelector("main");
-cats.forEach(function (cat) {
-  let card = `<div class="${
-    cat.favourite ? "card like" : "card"
-  }" style="background-image: url(${cat.img_link})">
-    <span>${cat.name}</span>
-    </div>`;
-  main.innerHTML += card;
-});
 
-let cards = document.getElementsByClassName("card");
-
-for (let i = 0, cnt = cards.length; i < cnt; i++) {
-  const width = cards[i].offsetWidth;
-  cards[i].style.height = width * 0.6 + "px";
-}
+const updCards = function (data) {
+  main.innerHTML = "";
+  data.forEach(function (cat) {
+    if (cat.id) {
+      let card = `<div class="${
+        cat.favourite ? "card like" : "card"
+      }" style="background-image:
+  url(${cat.img_link || "images/cat.jpg"})">
+  <span>${cat.name}</span>
+  </div>`;
+      main.innerHTML += card;
+    }
+  });
+  let cards = document.getElementsByClassName("card");
+  for (let i = 0, cnt = cards.length; i < cnt; i++) {
+    const width = cards[i].offsetWidth;
+    cards[i].style.height = width * 0.6 + "px";
+  }
+};
 
 let addBtn = document.querySelector("#add");
 let popupForm = document.querySelector("#popup-form");
@@ -29,3 +34,53 @@ closePopupForm.addEventListener("click", () => {
   popupForm.classList.remove("active");
   popupForm.parentElement.classList.remove("active");
 });
+
+const api = new Api("nata_kers");
+
+let form = document.forms[0];
+form.img_link.addEventListener("change", (e) => {
+  form.firstElementChild.style.backgroundImage = `url(${e.target.value})`;
+});
+form.img_link.addEventListener("input", (e) => {
+  form.firstElementChild.style.backgroundImage = `url(${e.target.value})`;
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let body = {};
+  for (let i = 0; i < form.elements.length; i++) {
+    let inp = form.elements[i];
+    if (inp.type === "checkbox") {
+      body[inp.name] = inp.checked;
+    } else if (inp.name && inp.value) {
+      if (inp.type === "number") {
+        body[inp.name] = +inp.value;
+      } else {
+        body[inp.name] = inp.value;
+      }
+    }
+  }
+  console.log(body);
+  api
+    .addCat(body)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.message === "ok") {
+        form.reset();
+        closePopupForm.click();
+      } else {
+        console.log(data);
+      }
+    });
+});
+
+const getCats = function (api) {
+  api
+    .getCats()
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.message === "ok") {
+        updCards(data.data);
+      }
+    });
+};
+getCats(api);
